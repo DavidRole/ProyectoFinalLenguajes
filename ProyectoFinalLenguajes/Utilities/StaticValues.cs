12e1214d9bc1df6a2bef0b89aa76a115e11fdb2e
DavidRole <david.josue.djrl@gmail.com>
2025-07-01 17:28:00 -0600

    Added static values

--- ProyectoFinalLenguajes/Utilities/StaticValues.cs
+++ ProyectoFinalLenguajes/Utilities/StaticValues.cs
@@ -2,8 +2,16 @@
 {
     public static class StaticValues
     {
+        // Valores para roles 
         public const string RoleAdmin = "Admin";
         public const string RoleCook = "Cook";
         public const string RoleCustomer = "Customer";
+
+
+        // Valores para Entregas 
+        public const String ActiveOrder = "Active";
+        public const String DelioveredOrder = "Delivered";
+        public const String NulledOrder = "Nulled";
+
     }
 }
